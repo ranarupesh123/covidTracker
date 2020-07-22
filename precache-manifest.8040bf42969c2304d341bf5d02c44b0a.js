b868acab6e47b722ec315fd491dab0d755aaad88
ranarupesh <rupeshrana288@gamil.com>
2020-07-23 00:08:32 +0530

    Updates

---
+++ precache-manifest.8040bf42969c2304d341bf5d02c44b0a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1df788ab89bf7048b321d1d7d479e8b8",
+    "url": "/covidTracker/index.html"
+  },
+  {
+    "revision": "b9782a68645d6b511be0",
+    "url": "/covidTracker/static/css/main.743d40d5.chunk.css"
+  },
+  {
+    "revision": "d3f7956113eda869a1c7",
+    "url": "/covidTracker/static/js/2.bef6128d.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covidTracker/static/js/2.bef6128d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9782a68645d6b511be0",
+    "url": "/covidTracker/static/js/main.b3e9fafd.chunk.js"
+  },
+  {
+    "revision": "f04f4299982413a55661",
+    "url": "/covidTracker/static/js/runtime-main.f0c9259b.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covidTracker/static/media/image.d7265326.png"
+  }
+]);
